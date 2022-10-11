21442094c395726bfa4dc27dbe50e2430c0834fb
tobidechamp15@gmail.com <Olubukola.Oluwadare@ubagroup.com>
2022-10-11 14:48:11 +0100

    added-questions

---
+++ index.js
@@ -0,0 +1,13 @@
+//ASSIGNMENTS
+// 1. Create a function that takes an array of numbers and returns the max number in the array.
+// 2. Create a function that takes an array of numbers and returns the min number in the array.
+// 3. Create a function that takes an array of numbers and returns the sum of all the numbers in the array.
+// 4. Create a function that takes an array of numbers and returns the average of all the numbers in the array.
+// 5. Create a function that takes an array of numbers and returns the sum of all the even numbers in the array.
+// 6. Create a function that takes an array of numbers and returns the sum of all the odd numbers in the array.
+// 7. Create a function that takes an array of numbers and returns the sum of all the numbers divisible by 5 in the array.
+// 8. Create a function that takes an array of strings and returns the longest string in the array.
+// 9. Create a function that takes an array of strings and returns the shortest string in the array.
+// 10. Create a function that takes an array of numbers and filters out all the odd numbers in the array.
+// 11. Create a function that takes an array of numbers and filters out all the even numbers in the array.
+// 12. Create a function that takes an array of strings and returns the array with all the strings capitalized.
