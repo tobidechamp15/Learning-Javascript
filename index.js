755f329475eda35215abeb15003be93278825ced
tobidechamp15@gmail.com <Olubukola.Oluwadare@ubagroup.com>
2022-10-11 15:36:26 +0100

    assignments

--- index.js
+++ index.js
@@ -11,3 +11,15 @@
 // 10. Create a function that takes an array of numbers and filters out all the odd numbers in the array.
 // 11. Create a function that takes an array of numbers and filters out all the even numbers in the array.
 // 12. Create a function that takes an array of strings and returns the array with all the strings capitalized.
+
+//No. 1
+// 1. Create a function that takes an array of numbers and returns the max number in the array.
+console.log(Math.max.apply(null, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+// //This method is called the Javascript function which allows you to find the maximum number in an array.
+
+//2. Create a function that takes an array of numbers and returns the min number in the array.
+console.log(Math.min.apply(null, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+// //This method is called the Javascript function which allows you to find the maximum number in an array.
+//3. Create a function that takes an array of numbers and returns the sum of all the numbers in the array.
+
+// 4. Create a function that takes an array of numbers and returns the average of all the numbers in the array.
